b842b8392fd0a5e49d4bfb78834dc311ff5a370d
gjansen <geert__jansen@hotmail.com>
2018-05-16 18:20:08 +0200

    From a tutorial. DOESN'T WORK YET.

--- DragAndDropWpfExample/DragAndDropWpfExample/MainWindow.xaml.cs
+++ DragAndDropWpfExample/DragAndDropWpfExample/MainWindow.xaml.cs
@@ -20,9 +20,80 @@ namespace DragAndDropWpfExample
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point startPoint;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void List_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Store the mouse position
+            startPoint = e.GetPosition(null);
+
+        }
+
+        private void List_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = startPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)
+            {
+                // Get the dragged ListViewItem
+                ListView listView = sender as ListView;
+                ListViewItem listViewItem =
+                    FindAnchestor<ListViewItem>((DependencyObject)e.OriginalSource);
+
+                // Find the data behind the ListViewItem
+                string contact = (string)listView.ItemContainerGenerator.
+                    ItemFromContainer(listViewItem);
+
+                // Initialize the drag & drop operation
+                DataObject dragData = new DataObject("myFormat", contact);
+                DragDrop.DoDragDrop(listViewItem, dragData, DragDropEffects.Move);
+            }
+        }
+
+        // Helper to search up the VisualTree
+        private static T FindAnchestor<T>(DependencyObject current)
+            where T : DependencyObject
+        {
+            do
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            }
+            while (current != null);
+            return null;
+        }
+
+        private void DropList_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent("myFormat"))
+            {
+                string contact = e.Data.GetData("myFormat") as string;
+                ListView listView = sender as ListView;
+                listView.Items.Add(contact);
+            }
+
+        }
+
+        private void DropList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent("myFormat") ||
+        sender == e.Source)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+
         }
     }
 }
